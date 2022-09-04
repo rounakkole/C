5d00e42b691f49509d12b238dff1a4ffffe6c7d5
rounak <rounakkole@gmail.com>
2022-09-04 12:25:06 +0530

    initial commit

---
+++ Random_Number/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//replit.com/@rounakkole
+
+int main()
+{
+	int i = 0;
+  int rn;
+  double regex;
+	double time_taken;
+	clock_t t1, t2;
+
+	t1 = clock();
+	for (i; i < 100; i++)
+	{
+		system("clear");
+		printf("Time taking to execute  %d\n", i);
+	}
+
+	t1 = clock() - t1;
+
+	i = 0;
+
+	t2 = clock();
+	for (i; i < 10000; i++)
+	{
+		printf("\e[1;1H\e[2J");
+		printf("Time taken to execute %d\n", i);
+	}
+
+
+	t2 = clock() - t2;
+
+
+	printf("Time taken by system\(\"clear\") %f\n",
+		((double)t1) / CLOCKS_PER_SEC);
+	printf("Time taken regex %f",
+		((double)t2) / CLOCKS_PER_SEC);
+  regex = 	((double)t2) / CLOCKS_PER_SEC * 1000000;
+  rn = regex;
+
+  printf("\n \n \n RANDOM NUMBER     %d \n", rn);
+
+	return 0;
+}
---
+++ Super_Calculator/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+//replit.com/@rounakkole
+int main(void) {
+long num1,num2;
+char oper;
+long ans;
+int i=1;
+system("clear");
+//printf("example :: enter expression: 2+3  \nintegers only | + - * / \n \n");
+
+printf("\n  SUPER CALCULATOR\n _____________________\n|  _________________  |\n| |            2+3  | |\n| |_________________| |\n|  ___ ___ ___   ___  |\n| | 7 | 8 | 9 | | + | |\n| |___|___|___| |___| |\n| | 4 | 5 | 6 | | - | |\n| |___|___|___| |___| |\n| | 1 | 2 | 3 | | x | |\n| |___|___|___| |___| |\n| | C | 0 | = | | / | |\n| |___|___|___| |___| |\n|_____________________|\n");
+
+while(i<100){
+
+  printf("\n ENTER EXPRESSION:    ");
+
+
+  
+
+
+
+  char str[50];
+  scanf("%[+]%*c", str);
+  scanf("%ld",&num1);
+  scanf(" %c",&oper);
+  scanf("%ld",&num2);
+  
+  system("clear");
+
+
+  
+  printf("\n _____________________\n|  _________________  |\n| |                 | |\n");
+  
+  printf("| | %ld %c %ld =    ", num1,oper,num2);
+   
+  if(oper == '+'){
+    printf("%ld",num1 + num2);
+  }
+  else if(oper == '-'){
+    printf("%ld",num1 - num2);
+  }
+  else if(oper == '*'){
+    printf("%ld",num1 * num2);
+  }
+  else if(oper == '/'){
+    printf("%ld",num1 / num2);
+    if((num1 % num2) >= 1)
+      printf("\n| |(remainder %ld) ", num1 % num2);
+  }
+
+  
+  printf("\n| |_________________| |\n|  ___ ___ ___   ___  |\n| | 7 | 8 | 9 | | + | |\n| |___|___|___| |___| |\n| | 4 | 5 | 6 | | - | |\n| |___|___|___| |___| |\n| | 1 | 2 | 3 | | x | |\n| |___|___|___| |___| |\n| | C | 0 | = | | / | |\n| |___|___|___| |___| |\n|_____________________|\n");
+
+
+
+          printf("\n");
+++i;
+}
+
+return 0;
+}
